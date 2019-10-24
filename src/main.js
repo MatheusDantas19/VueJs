e12f12fca1c19fbf162c2817c13ebfea28b090db
Matheus Inacio Nascimento Dantas <mind@icomp.ufam.edu.br>
2019-10-24 18:57:01 -0400

    up

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import BootstrapVue  from "bootstrap-vue"; 
+import VueResource from "vue-resource"
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(VueResource);
+Vue.use(BootstrapVue);
+
 new Vue({
   el: '#app',
   render: h => h(App)
